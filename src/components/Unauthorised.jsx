5f6b2ecef14b85ea97a86c53f5c7f12a55f38c71
Austinps <aptstack@gmail.com>
2023-05-20 21:08:56 +0200

    add editor and admin pages plus navigation

--- src/components/Unauthorised.jsx
+++ src/components/Unauthorised.jsx
@@ -1,5 +1,20 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Unauthorised() {
-  return <div>Unauthorised</div>;
-}
+const Unauthorized = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => navigate(-1);
+
+  return (
+    <section>
+      <h1>Unauthorized</h1>
+      <br />
+      <p>You do not have access to the requested page.</p>
+      <div className="flexGrow">
+        <button onClick={goBack}>Go Back</button>
+      </div>
+    </section>
+  );
+};
+
+export default Unauthorized;
