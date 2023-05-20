91e1c193f840961aeff48fe4c26d43c7302b7fd5
Austinps <aptstack@gmail.com>
2023-05-20 20:54:57 +0200

    add axios file to api folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import {
+  Admin,
   Register,
   Login,
   Home,
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
+export { default as Admin } from "./Admin";
 export { default as Register } from "./Register";
 export { default as Login } from "./Login";
 export { default as Home } from "./Home";
 export { default as Layout } from "./Layout";
 export { default as Editor } from "./Editor";
-export { default as Admin } from "./Admin";
+
 export { default as Missing } from "./Missing";
 export { default as Unauthorized } from "./Unauthorised";
 export { default as Lounge } from "./Lounge";
